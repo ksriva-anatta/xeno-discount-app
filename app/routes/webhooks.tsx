41372dcb039aac582f15b23680c0ef29f0530ed9
ksriva-anatta <kislay@anattadesign.com>
2024-02-24 22:19:49 +0530

    updated webhook

--- app/routes/webhooks.tsx
+++ app/routes/webhooks.tsx
@@ -1,9 +1,9 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
+// import db from "../db.server";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
-  const { topic, shop, session, admin } = await authenticate.webhook(request);
+  const { topic, admin } = await authenticate.webhook(request);
 
   if (!admin) {
     // The admin context isn't returned if the webhook fired after a shop was uninstalled.
@@ -12,9 +12,9 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
   switch (topic) {
     case "APP_UNINSTALLED":
-      if (session) {
-        await db.session.deleteMany({ where: { shop } });
-      }
+      // if (session) {
+      //   await db.session.deleteMany({ where: { shop } });
+      // }
 
       break;
     case "CUSTOMERS_DATA_REQUEST":
